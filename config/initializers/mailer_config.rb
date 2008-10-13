2cd32e69dadd4f5b1cb8c23b90f16c8570e541c6
Frederico Araujo <fred.the.master@gmail.com>
2008-10-13 23:44:51 +0700

    fixed the email thing

--- config/initializers/mailer_config.rb
+++ config/initializers/mailer_config.rb
@@ -1,11 +1,10 @@
-ActionMailer::DeliveryMethod = :sendmail
-ActionMailer::SmtpSettings = {
+ActionMailer::Base.delivery_method = :sendmail
+ActionMailer::Base.smtp_settings = {
   :address        => "127.0.0.1",
   :port           => 25,
   :domain         => "www.comunidad-catolica.com"
 }
-
 ActionMailer::Base.sendmail_settings = {
   :location       => '/usr/sbin/sendmail',
-  :arguments      => '-i -t -f admin@comunidad-catolica.com'
+  :arguments      => '-i -t -f info@comunidad-catolica.com'
 }

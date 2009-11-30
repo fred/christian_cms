d880ea5290c1a5754556f1ab90a2dbf21e445fb9
Frederico Araujo <fred.the.master@gmail.com>
2009-11-30 16:44:48 +0700

    changin gems from github to gemcutter

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -23,21 +23,16 @@ Rails::Initializer.run do |config|
   # Specify gems that this application depends on. 
   # They can then be installed with "rake gems:install" on new installations.
   # You have to specify the <tt>:lib</tt> option for libraries, where the Gem name (<em>sqlite3-ruby</em>) differs from the file itself (_sqlite3_)
-  # config.gem "bj"
-  # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
-  # config.gem "sqlite3-ruby", :lib => "sqlite3"
-  
-  config.gem 'mislav-will_paginate', :lib => 'will_paginate', :source => 'http://gems.github.com'
+  config.gem 'will_paginate'
   config.gem 'mini_magick'
   config.gem 'RedCloth'
   config.gem 'json'
   config.gem 'feedtools', :lib => 'feed_tools'
-  # Calendars
   config.gem "calendar_date_select"
   config.gem 'haml'
-  # Testing, FactoryGirl
-  config.gem "thoughtbot-factory_girl",   :lib => "factory_girl",   :source => "http://gems.github.com"
-    
+  config.gem "factory_girl"
+  config.gem "ajaxful_rating"
+  
   # These cause problems with irb. Left in for reference
   # config.gem 'rspec-rails', :lib => 'spec/rails', :version => '1.1.11'
   # config.gem 'rspec', :lib => 'spec', :version => '1.1.11'

0c9c4c372b7d3b6d8586205ee3ade2644a27f25c
Frederico Araujo <fred.the.master@gmail.com>
2009-11-30 16:31:35 +0700

    changin gems from github to gemcutter, minor changes to translations

--- vendor/plugins/ajaxful-rating/generators/ajaxful_rating/templates/model.rb
+++
@@ -1,6 +0,0 @@
-class Rate < ActiveRecord::Base
-  belongs_to :<%= file_name %>
-  belongs_to :rateable, :polymorphic => true
-  
-  attr_accessible :rate, :dimension
-end
--- vendor/plugins/ajaxful-rating/init.rb
+++
@@ -1,5 +0,0 @@
-require 'ajaxful_rating'
-require 'ajaxful_rating_helper'
-
-ActiveRecord::Base.send(:include, AjaxfulRating) unless ActiveRecord::Base.include?(AjaxfulRating)
-ActionView::Base.send(:include, AjaxfulRating::Helper) unless ActionView::Base.include?(AjaxfulRating::Helper)

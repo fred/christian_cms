3bcb2addaedb12b88cc8aa5cf7dde8a68727b948
Frederico Araujo <fred.the.master@gmail.com>
2010-08-04 16:20:41 +0700

    checked to ckeditor, removed acts-as-taggable due to blocking bug

---
+++ public/ckeditor/config.js
@@ -0,0 +1,18 @@
+﻿/*
+Copyright (c) 2003-2010, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.editorConfig = function( config )
+{
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+	config.entities : false;
+	config.contentsLanguage : 'es';
+	config.entities_latin : false;
+	config.entities_processNumerical : false;
+	config.emailProtection : 'encode';
+	config.forcePasteAsPlainText = true;
+	config.height = '400px';
+};
